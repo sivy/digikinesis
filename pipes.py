054fe457c141b00ddd576c906a342511486faf6c
Steve Ivy <steveivy@gmail.com>
2013-04-23 11:43:36 -0700

    still hacking. why isn't the rotate rotating around world 0,0,0?

--- pipes.py
+++ pipes.py
@@ -15,24 +15,25 @@ def pipe(radius, height, name=None):
     return val
 
 
-def make_pipe(new_radius, new_height, from_pipe=None, name=None):
-    if from_pipe:
-        ret = cmds.duplicate(from_pipe, rr=True, name=name, un=True)
+def make_pipe(new_radius, new_height, basename="pipe_000", from_pipe_name=None):
+    t = 1/float(16)  # 16ga
+    if from_pipe_name:
+        ret = cmds.duplicate(from_pipe_name, un=True)
         log.debug(ret)
-        new_name, shape = ret
+        new_name = ret[0]
+        shape = ret[-1]
     else:
-        new_name, shape = cmds.polyPipe(name=name)
+        new_name, shape = cmds.polyPipe(n=basename, t=t)
 
-    log.debug("copied: " + new_name if from_pipe else "new: " + new_name)
+    log.debug("copied: " + new_name if from_pipe_name else "new: " + new_name)
     cmds.setAttr(shape + '.radius', new_radius)
     cmds.setAttr(shape + '.height', new_height)
-    cmds.setAttr(shape + '.radius', new_radius)
     return new_name, shape
 
 # thing = pipe(1, 6)
 
-n_pipes = 20
-n_divisions = 10
+n_pipes = 3
+# n_divisions = 10
 vert_xlate = 3
 
 min_piped = 1
@@ -45,29 +46,32 @@ rotated_deg = 0
 last_pipe_d = 0
 last_pipe_name = None
 
-for i in range(1, n_pipes):
-    n = "p%d" % i
-    pd = random.choice(range(min_piped, max_piped+1))
+for i in range(1, n_pipes+1):
+    p_name = "pipe%d" % i
+    pd = 2  # random.choice(range(min_piped, max_piped+1))
 
-    p_name, p_shape = make_pipe(pd, 6.0, name=n, from_pipe=last_pipe_name)
+    p_name, p_shape = make_pipe(pd, 6.0, from_pipe_name=last_pipe_name, basename=p_name)
     cmds.select(p_name, r=True)
     # translate away from center?
-    cmds.xform(t=[main_radius, 0, 0], ws=True)
+    cmds.xform(t=[0, 0, main_radius], ws=True)
     # reset just the translation context
     cmds.makeIdentity(apply=True, t=1, r=1, s=0, n=0)
 
-    # cmds.xform(t=[3, 0, 0])
-    rotate_distance = last_pipe_d/2 + pd/2
-    rotate_degrees = (360 * rotate_distance)/main_c
+    if not i == 1:
+        # cmds.xform(t=[3, 0, 0])
+        rotate_distance = last_pipe_d/2 + pd/2
+        rotate_degrees = (360 * rotate_distance)/main_c
 
-    rotated_deg = rotated_deg + rotate_degrees
+        rotated_deg = rotated_deg + rotate_degrees
 
-    cmds.rotate(0, rotate_degrees, 0, p=[0, 0, 0])
+        # cmds.rotate(0, rotate_degrees, 0, p=[0, 0, 0])
+        log.debug('rotating %s by %d degrees around p [0 0 0] in worldspace' % (p_name, rotate_degrees))
+        cmds.rotate(0, rotate_degrees, 0, p_name, p=[0, 0, 0], ws=True)
 
     # translate vertically
-    cmds.xform(t=[0, vert_xlate*i, 0], ws=True)
+    # cmds.xform(t=[0, vert_xlate*i, 0], ws=True)
     # reset just the translation context
-    cmds.makeIdentity(apply=True, t=1, r=0, s=0, n=0)
+    # cmds.makeIdentity(apply=True, t=1, r=1, s=0, n=0)
 
     last_pipe_name = p_name
     last_pipe_d = pd
